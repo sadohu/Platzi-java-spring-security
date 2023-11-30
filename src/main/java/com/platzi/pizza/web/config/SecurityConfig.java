c71476de24447be8567ed22e4127a589bb9cb42a
sadohu <sadohu26@yahoo.com>
2023-11-30 10:40:22 -0500

    Disable CSRF in SecurityConfig

--- src/main/java/com/platzi/pizza/web/config/SecurityConfig.java
+++ src/main/java/com/platzi/pizza/web/config/SecurityConfig.java
@@ -1,13 +1,19 @@
 package com.platzi.pizza.web.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        // Deshabilitar CSRF:
+        httpSecurity.csrf(AbstractHttpConfigurer::disable);
+
         httpSecurity
                 // Autorizar todas las peticiones:
                 .authorizeRequests()
