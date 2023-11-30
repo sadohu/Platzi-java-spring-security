4db4e4dacd8b8c8f0ee848d683a7c2e992f89e45
sadohu <sadohu26@yahoo.com>
2023-11-30 16:09:15 -0500

    [Add] requestMatchers (permit - deny)

--- src/main/java/com/platzi/pizza/web/config/SecurityConfig.java
+++ src/main/java/com/platzi/pizza/web/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.platzi.pizza.web.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
@@ -12,14 +13,24 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
         // Deshabilitar CSRF:
-        httpSecurity.csrf(AbstractHttpConfigurer::disable);
+        // httpSecurity.csrf(AbstractHttpConfigurer::disable);
 
         httpSecurity
+                // Deshabilitar CSRF:
+                .csrf().disable()
                 // Deshabilitar CORS:
                 .cors().and()
 
                 // Autorizar todas las peticiones:
                 .authorizeRequests()
+
+                // Permitir peticiones GET a /api/* (Primer nivel) o /api/** (Cualquier
+                // subruta):
+                // .requestMatchers(HttpMethod.GET, "/api/**").permitAll()
+                .requestMatchers(HttpMethod.GET, "/api/pizzas/**").permitAll()
+
+                // Denegar peticiones PUT a todo el proyecto:
+                .requestMatchers(HttpMethod.PUT).denyAll()
 
                 // Cualquier peticiones:
                 .anyRequest()
