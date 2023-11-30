0339c89492aec90fcbd54bb4402cee6fc0da7bbb
sadohu <sadohu26@yahoo.com>
2023-11-30 16:27:13 -0500

    Add user authentication and password encryption

--- src/main/java/com/platzi/pizza/web/config/SecurityConfig.java
+++ src/main/java/com/platzi/pizza/web/config/SecurityConfig.java
@@ -5,6 +5,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -46,5 +52,33 @@ public class SecurityConfig {
                 // Se requiere autenticación básica:
                 .httpBasic();
         return httpSecurity.build();
+    }
+
+    @Bean
+    public UserDetailsService memoryUsers() {
+        /**
+         * Crear un usuario en memoria, con sus propias credenciales
+         * De esta manera sustituimos el usuario por defecto de Spring Security
+         * Por ultimo, Sring Security no generará una contraseña aleatoria.
+         */
+
+        UserDetails admin = User.builder()
+                .username("admin")
+                /**
+                 * .password("admin")
+                 * No se recomienda guardar la contraseña en texto plano
+                 * Por lo que se debe encriptar
+                 */
+
+                .password(passwordEncoder().encode("admin"))
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
